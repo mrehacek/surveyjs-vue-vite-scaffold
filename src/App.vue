300242aa50df69e9fe5564fa9433283dc7c11fb6
Marko Rehacek <marko.rehacek@gmail.com>
2022-03-19 15:55:58 +0100

    Add code

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Survey :survey="survey" />
+</template>
+
+<script>
+import "survey-vue/modern.min.css";
+import { StylesManager, Model, Survey } from "survey-vue";
+
+StylesManager.applyTheme("modern");
+
+const surveyJson = {
+  elements: [
+    {
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text",
+    },
+    {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text",
+    },
+  ],
+};
+
+export default {
+    components: {
+    Survey
+  },
+  data() {
+    const survey = new Model(surveyJson);
+    return {
+      survey,
+    };
+  },
+};
+</script>
